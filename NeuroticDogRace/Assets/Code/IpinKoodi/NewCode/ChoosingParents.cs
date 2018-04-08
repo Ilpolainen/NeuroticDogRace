995f1f6b909df59fd21d96991803d28cc2e3205f
Ilpolainen <pohjolanilmari@gmail.com>
2018-04-08 10:51:36 +0300

    Cleaning

---
+++ NeuroticDogRace/Assets/Code/IpinKoodi/NewCode/ChoosingParents.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoosingParents : GameState {
+        void Start()
+        {
+
+        }
+
+        public void Wee()
+        {
+            Debug.Log(this.touched);
+        }
+}
+
---
+++ NeuroticDogRace/Assets/Code/IpinKoodi/NewCode/Selector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selector : MonoBehaviour {
+
+    GameState state;
+    Renderer[] renderers;
+
+    void Start()
+    {
+        state = null;
+        renderers = gameObject.transform.GetComponentsInParent<Transform>()[1].GetComponentsInChildren<Renderer>();
+    }
+
+    void OnMouseDown(){
+        //state.SetTouched(gameObject);
+        SetTouchColor(Color.blue);
+    }
+
+    void OnMouseUp()
+    {
+        SetTouchColor(Color.red);
+    }
+
+    void SetTouchColor(Color color)
+    {
+        foreach (Renderer rend in renderers)
+        {
+            Material[] mats = rend.materials;
+            foreach (Material mat in mats)
+            {
+                mat.color = color;
+            }
+        }
+    }
+}
